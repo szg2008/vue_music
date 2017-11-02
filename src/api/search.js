3977fdcdae07f73bb32d8aa5608b49e4d46a8fa5
szg2008 <l>
2017-11-02 20:15:12 +0800

    search

---
+++ src/api/search.js
@@ -0,0 +1,15 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams,options} from './config'
+
+export function getHotKey(){
+    const url = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg'
+
+    const data = Object.assign({},commonParams,{
+        g_tk:'1756559080',
+        format:'json',
+        platform:'h5',
+        needNewCode:1
+    })
+
+    return jsonp(url,data,options)
+}
---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,65 @@
+<template lang="html">
+    <div class="search-box">
+        <i class="icon-box"></i>
+        <input type="text" name="" value="" class="box" :placeholder="placeholder" v-model="query">
+        <i class="icon-dismiss" v-show="query" @click="clear"></i>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        placeholder:{
+            type:String,
+            default:'搜素歌曲、歌手'
+        }
+    },
+    data(){
+        return {
+            query:''
+        }
+    },
+    methods:{
+        clear(){
+            this.query = ''
+        },
+        setQuery(query){//向外暴露一个方法以供调用
+            this.query = query
+        }
+    },
+    created(){
+        //可以直接写成watch
+        this.$watch('query',(newQuery) => {
+            this.$emit('query',newQuery)//对外派发一个事件
+        })
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/variable'
+.search-box
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    width: 100%
+    padding: 0 6px
+    height: 40px
+    background: $color-highlight-background
+    border-radius: 6px
+    .icon-search
+        font-size: 24px
+        color: $color-background
+    .box
+        flex: 1
+        margin: 0 5px
+        line-height: 18px
+        background: $color-highlight-background
+        color: $color-text
+        font-size: $font-size-medium
+        &::placeholder
+            color: $color-text-d
+    .icon-dismiss
+        font-size: 16px
+        color: $color-background
+</style>
